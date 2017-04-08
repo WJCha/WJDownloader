8b0a0ab6bf8e820108e02ee4508c3ab4aeef40ab
陈威杰 <chenweijie@WJCha.local>
2017-04-08 22:16:34 +0800

    项目初始化

---
+++ WJDownloaderDemo/WJDownloaderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WJDownloaderDemo
+//
+//  Created by 陈威杰 on 2017/4/8.
+//  Copyright © 2017年 ChenWeiJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WJDownloaderDemo/WJDownloaderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJDownloaderDemo
+//
+//  Created by 陈威杰 on 2017/4/8.
+//  Copyright © 2017年 ChenWeiJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
